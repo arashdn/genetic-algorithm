f3e164dbb581f144bc527d484d31e22371e3b7ec
Arashdn <arash@dargahi.org>
2015-05-28 20:29:55 +0430

    Charts added

--- src/genetic_algorithm/Main.java
+++ src/genetic_algorithm/Main.java
@@ -1,5 +1,12 @@
 package genetic_algorithm;
 
+import javafx.application.Platform;
+import javafx.embed.swing.JFXPanel;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.chart.*;
+import javax.swing.JFrame;
+
 /**
  *
  * @author Arash
@@ -19,26 +26,165 @@ public class Main
         
         Chromosome.setPolynomial(pn);
 
-        Gene.maxValue = 50;
-        Gene.minValue = -50;
+        Gene.maxValue = 100;
+        Gene.minValue = -100;
         Gene.points = 1;
         
-        int generations = 100;
+        int generations = 50;
+        final int types = 8;
         
         Chromosome.setCrossOverType(Chromosome.CO_TYPE_DOUBLE_POINT);
         GeneticAlgorithm.setSelectionType(GeneticAlgorithm.SELECTION_TYPE_ROULETTE);
         GeneticAlgorithm.setAddExisting(false);
         
-        GeneticAlgorithm ga = new GeneticAlgorithm(600, 75, 30, 1, 30);
-        System.out.println(ga.toString());
+        GeneticAlgorithm ga;
+        //System.out.println(ga.toString());
         
-        for (int i = 0; i < generations; i++)
+        double [][] best = new double[types][generations];
+        double [][] avg = new double[types][generations];
+        String [] names = new String[types];
+        
+        for (int j = 0; j < types; j++)
         {
-            ga.repeat();
+            switch(j)
+            {
+                case 0:
+                    Chromosome.setCrossOverType(Chromosome.CO_TYPE_SINGLE_POINT);
+                    GeneticAlgorithm.setSelectionType(GeneticAlgorithm.SELECTION_TYPE_TOURNAMENT);
+                    GeneticAlgorithm.setAddExisting(true);
+                    names[j] = "Single CO - Tournament - Allow Repetitive";
+                    break;
+                case 1:
+                    Chromosome.setCrossOverType(Chromosome.CO_TYPE_SINGLE_POINT);
+                    GeneticAlgorithm.setSelectionType(GeneticAlgorithm.SELECTION_TYPE_TOURNAMENT);
+                    GeneticAlgorithm.setAddExisting(false);
+                    names[j] = "Single CO - Tournament - Not Repetitive";
+                    break;
+                case 2:
+                    Chromosome.setCrossOverType(Chromosome.CO_TYPE_SINGLE_POINT);
+                    GeneticAlgorithm.setSelectionType(GeneticAlgorithm.SELECTION_TYPE_ROULETTE);
+                    GeneticAlgorithm.setAddExisting(true);
+                    names[j] = "Single CO - Roulete - Allow Repetitive";
+                    break;
+                case 3:
+                    Chromosome.setCrossOverType(Chromosome.CO_TYPE_SINGLE_POINT);
+                    GeneticAlgorithm.setSelectionType(GeneticAlgorithm.SELECTION_TYPE_ROULETTE);
+                    GeneticAlgorithm.setAddExisting(false);
+                    names[j] = "Single CO - Roulete - NOT Repetitive";
+                    break;
+                case 4:
+                    Chromosome.setCrossOverType(Chromosome.CO_TYPE_DOUBLE_POINT);
+                    GeneticAlgorithm.setSelectionType(GeneticAlgorithm.SELECTION_TYPE_TOURNAMENT);
+                    GeneticAlgorithm.setAddExisting(true);
+                    names[j] = "Double CO - Tournament - Allow Repetitive";
+                    break;
+                case 5:
+                    Chromosome.setCrossOverType(Chromosome.CO_TYPE_DOUBLE_POINT);
+                    GeneticAlgorithm.setSelectionType(GeneticAlgorithm.SELECTION_TYPE_TOURNAMENT);
+                    GeneticAlgorithm.setAddExisting(false);
+                    names[j] = "Double CO - Tournament - Not Repetitive";
+                    break;
+                case 6:
+                    Chromosome.setCrossOverType(Chromosome.CO_TYPE_DOUBLE_POINT);
+                    GeneticAlgorithm.setSelectionType(GeneticAlgorithm.SELECTION_TYPE_ROULETTE);
+                    GeneticAlgorithm.setAddExisting(true);
+                    names[j] = "Double CO - Roulete - Allow Repetitive";
+                    break;
+                case 7:
+                    Chromosome.setCrossOverType(Chromosome.CO_TYPE_DOUBLE_POINT);
+                    GeneticAlgorithm.setSelectionType(GeneticAlgorithm.SELECTION_TYPE_ROULETTE);
+                    GeneticAlgorithm.setAddExisting(false);
+                    names[j] = "Double CO - Roulete - NOT Repetitive";
+                    break;
+            }
+            
+            ga = new GeneticAlgorithm(600, 75, 20, 1, 30);
+            
+            for (int i = 0; i < generations; i++)
+            {
+                ga.repeat();
+                best[j][i] = ga.getBestChromosome().getFitness();
+                avg[j][i] = ga.getFitnessAverage();
+            }
         }
         
         
         
+        
+        
+        /////////////////GUI//////////////////
+        JFrame frame = new JFrame("Chart for Best Fitness");
+        final JFXPanel fxPanel = new JFXPanel();
+        frame.add(fxPanel);
+        frame.setSize(800, 630);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+        
+        
+        JFrame frame2 = new JFrame("Chart for Average Fitness");
+        final JFXPanel fxPanel2 = new JFXPanel();
+        frame2.add(fxPanel2);
+        frame2.setSize(800, 630);
+        frame2.setVisible(true);
+        frame2.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+
+
+
+        Platform.runLater(new Runnable() 
+        {
+            @Override
+            public void run() 
+            {
+                Scene scene = createChart(best,names);
+                fxPanel.setScene(scene);
+                Scene scene2 = createChart(avg,names);
+                fxPanel2.setScene(scene2);
+            }
+        });
+        
+    }
+    
+    
+    private static XYChart.Data getChartData(double x, String y) 
+    {
+        XYChart.Data data = new XYChart.Data<>();
+        data.setYValue(x);
+        data.setXValue(y);
+        return data;
+    }
+    
+    private static Scene createChart(double [][] inp , String [] names)
+    {
+        Group  root  =  new  Group();
+        Scene  scene  =  new  Scene(root, javafx.scene.paint.Color.ALICEBLUE);
+
+
+        NumberAxis lineYAxis= new NumberAxis();
+        CategoryAxis lineXAxis = new CategoryAxis();
+        lineYAxis.setLabel("Fitness");
+        lineXAxis.setLabel("Generation");
+        LineChart barChart = new LineChart(lineXAxis,lineYAxis);
+        barChart.setMinSize(800, 600);
+        
+
+        XYChart.Series [] bar = new XYChart.Series[names.length];
+        
+        for (int j = 0; j < names.length; j++)
+        {
+            bar[j] = new XYChart.Series<>();
+            bar[j].setName(names[j]);
+
+            for (int i = 0; i < inp[j].length ; i++) 
+            {
+                bar[j].getData().add(getChartData(inp[j][i], ""+(i+1)));
+            }
+            barChart.getData().addAll(bar[j]);
+        }
+        
+        
+        root.getChildren().add(barChart);
+
+        return (scene);
     }
     
 }
